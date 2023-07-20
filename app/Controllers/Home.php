bd4b65c1a13271d0c00e9ca24ad309d233ef01dd
GS <37976830+sotyarama@users.noreply.github.com>
2023-07-20 19:04:07 +0700

    try_dependent_dropdown [works]

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,52 @@
 
 namespace App\Controllers;
 
+use App\Models\CommonModel;
+
 class Home extends BaseController
 {
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new CommonModel();;
+    }
+
     public function index()
     {
-        return view('welcome_message');
+        $country = $this->model->selectData("countries");
+        // echo "<pre>";
+        // print_r($country);
+        // die();
+        $data['country'] = $country;
+        return view('welcome_message', $data);
+    }
+
+
+    public function state()
+    {
+        $countryID = $this->request->getPost("cId");
+        $stateData = $this->model->selectData("states", array("country_id" => $countryID));
+
+        // $output = "";
+        // foreach ($stateData as $state) {
+        //     $output .= "<option value='$state->id'>$state->name</option>";
+        // }
+        // echo json_encode($output);
+        echo json_encode($stateData);
+    }
+
+
+    public function city()
+    {
+        $stateID = $this->request->getPost("sId");
+        $cityData = $this->model->selectData("cities", array("state_id" => $stateID));
+
+        $output = "<option>select city</option>";
+        foreach ($cityData as $city) {
+            $output .= "<option value='$city->id'>$city->name</option>";
+        }
+        echo json_encode($output);
+        // echo json_encode($stateData);
     }
 }
